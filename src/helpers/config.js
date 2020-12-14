eb1a1ed75efca23702a578ca298cb4acbf0bc500
Muhammad Qadir <mahdimq@gmail.com>
2020-12-14 16:28:58 +0300

    add https

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -8,7 +8,7 @@ export const API_KEY = 'a19fde14eb2f33b545cd78125e338397';
 export const SEARCH_ENDPOINT = `${BASE_URL}search/movie?api_key=${API_KEY}&include_adult=false&query=`;
 export const POPULAR_ENDPOINT = `${BASE_URL}movie/popular?api_key=${API_KEY}&include_adult=false`;
 
-export const IMAGE_URL = 'http://image.tmdb.org/t/p/';
+export const IMAGE_URL = 'https://image.tmdb.org/t/p/';
 // Sizes: w300, w780, w1280, original
 export const backdrop_size = 'w1280';
 // w92, w154, w185, w342, w500, w780, original
